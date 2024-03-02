3234f91117f6973dc5c62b77cea6b5a711cdbcd6
LuisanArgoose <r.luzan@yandex.ru>
2024-03-02 13:43:58 +0300

    3 commit test

--- ShopProject.Tests/DbContextTests/ClientAPIDbContextTest.cs
+++ ShopProject.Tests/DbContextTests/ClientAPIDbContextTest.cs
@@ -20,7 +20,7 @@ namespace ShopProject.Tests.DataBaseTests
         [TestMethod]
         public async Task APISelectTest()
         {
-            _clientExample = new ClientAPIDbContext("https://localhost:7178/api/");
+           
             //await _clientExample.FillCollection();
             _clientExample.Categories.Load();
             var result = _clientExample.Categories.First();
@@ -29,7 +29,7 @@ namespace ShopProject.Tests.DataBaseTests
         [TestMethod]
         public async Task APICreateTest()
         {
-            _clientExample = new ClientAPIDbContext("https://localhost:7178/api/");
+            
             //await _clientExample.FillCollections();
             var testTable = new TestTable()
             {
