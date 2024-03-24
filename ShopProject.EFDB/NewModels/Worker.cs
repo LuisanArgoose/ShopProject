d3e433ebdcce032a7c1f81f6a7215c0b001ead9f
LuisanArgoose <r.luzan@yandex.ru>
2024-03-24 20:18:57 +0300

    Day-end commit

---
+++ ShopProject.EFDB/NewModels/Worker.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace ShopProject.EFDB.NewModels;
+
+public partial class Worker : ObservableObject
+{
+    [ObservableProperty]
+    private int _workerId;
+
+    [ObservableProperty]
+    private string _fullname = null!;
+
+    [ObservableProperty]
+    private string _login = null!;
+
+    [ObservableProperty]
+    private string _password = null!;
+
+    [ObservableProperty]
+    private int _workerTypeId;
+    private 
+    /*
+    [ObservableProperty]
+    private ICollection<OrderConsignment> _orderConsignments = new List<OrderConsignment>();
+
+    [ObservableProperty]
+    private ICollection<Payment> _paymentApprovierWorkers = new List<Payment>();
+
+    [ObservableProperty]
+	private ICollection<Payment> _paymentRecipientWorkers = new List<Payment>();
+
+    [ObservableProperty]
+	private ICollection<ProductConsignment> _productConsignments = new List<ProductConsignment>();
+
+    [ObservableProperty]
+	private ICollection<ProductOrder> _productOrders = new List<ProductOrder>();
+
+    [ObservableProperty]
+	private ICollection<Purchase> _purchases = new List<Purchase>();
+
+    [ObservableProperty]
+	private ICollection<Refund> _refunds = new List<Refund>();
+
+    [ObservableProperty]
+	private ICollection<ShopPosition> _shopPositions = new List<ShopPosition>();
+
+    [ObservableProperty]
+	private WorkerType _workerType = null!; */
+}
---
+++ ShopProject.EFDB/NewModels/WorkerType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopProject.EFDB.Models;
+
+public partial class WorkerType : ObservableObject
+{
+    [ObservableProperty]
+    private int _workerTypeId;
+
+    [ObservableProperty]
+    private string _workerTypeName = null!;
+
+    [ObservableProperty]
+    private ICollection<Worker> _workers  = new List<Worker>();
+}
